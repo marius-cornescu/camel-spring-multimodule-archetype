ddc27087bb71d6fd16657b46fd43daa2e209cfa3
Marius Cornescu <marius_cornescu@yahoo.com>
2020-07-05 13:30:13 +0200

    __ init __

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-__secondModule__/src/main/java/__secondModule__/camel/__secondModule__RouteBuilder.java
@@ -0,0 +1,49 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${secondModule}.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import static ${package}.common.ApplicationProperties.APP_NAME;
+import static org.apache.camel.LoggingLevel.INFO;
+
+@Component
+public class ${secondModule}RouteBuilder extends RouteBuilder {
+
+    static final String ROUTE_ID_TODO = "route." + APP_NAME + ".${secondModule}.fromTodo";
+    static final String NODE_ID_TODO_DEST = "node.toDestination";
+
+    private final long redeliveryDelay;
+    private final int maxRedeliveryAttempts;
+
+    private final int fromTimerPeriod;
+
+    @Autowired
+    public ${secondModule}RouteBuilder(
+            @Value("${symbol_dollar}{exception.redelivery.delay:5000}") long redeliveryDelay,
+            @Value("${symbol_dollar}{exception.redelivery.attempts:3}") int maxRedeliveryAttempts,
+            @Value("${symbol_dollar}{from.timer.period:50}") int fromTimerPeriod
+    ) {
+        this.redeliveryDelay = redeliveryDelay;
+        this.maxRedeliveryAttempts = maxRedeliveryAttempts;
+
+        this.fromTimerPeriod = fromTimerPeriod;
+    }
+
+    @Override
+    public void configure() {
+        onException(Exception.class)
+                .logRetryAttempted(true)
+                .redeliveryDelay(redeliveryDelay)
+                .maximumRedeliveries(maxRedeliveryAttempts);
+
+        from("timer:${secondModule}?period=" + fromTimerPeriod).routeId(ROUTE_ID_TODO)
+                .log(INFO, "Sending data")
+                .to("stream:out").id(NODE_ID_TODO_DEST);
+
+    }
+}
