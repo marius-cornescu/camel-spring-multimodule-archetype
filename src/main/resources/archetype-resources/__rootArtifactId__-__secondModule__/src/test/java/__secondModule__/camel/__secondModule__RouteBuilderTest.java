9c94c08f8edd4d4db24d173c328ec22899b6516c
Marius Cornescu <marius_cornescu@yahoo.com>
2020-07-05 13:40:17 +0200

    Two modules dynamic names

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-__secondModule__/src/test/java/__secondModule__/camel/__secondModule__RouteBuilderTest.java
@@ -0,0 +1,45 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${secondModule}.camel;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.builder.NotifyBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.apache.camel.test.spring.EnableRouteCoverage;
+import org.apache.camel.test.spring.MockEndpoints;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(CamelSpringBootRunner.class)
+@EnableRouteCoverage
+@MockEndpoints("log:foo") // mock the log:foo endpoint => mock:log:foo which we then use in the testing
+public class ${secondModule}RouteBuilderTest {
+
+    @Autowired
+    private CamelContext camelContext;
+
+    @EndpointInject("mock:log:foo")
+    private MockEndpoint mock;
+
+    @Test
+    public void shouldSayFoo() throws Exception {
+        mock.expectedBodiesReceived("Hello foo");
+
+        // we expect that one or more messages is automatic done by the Camel
+        // route as it uses a timer to trigger
+        NotifyBuilder notify = new NotifyBuilder(camelContext).whenDone(1).create();
+
+        assertTrue(notify.matches(10, TimeUnit.SECONDS));
+
+        mock.assertIsSatisfied();
+    }
+
+}
